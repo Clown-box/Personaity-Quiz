debab5818a21441f2e185505d0e003d236947a36
Clown-Box <Sm3n26@gmail.com>
2024-03-18 11:13:49 -0400

    Initial Commit

---
+++ Personaity Quiz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Personaity Quiz
+//
+//  Created by 6f on 3/18/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
